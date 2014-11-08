fd9471694a183b2813ac34354998f86941ff4103
nelsoncheng <standbackimgoingtotryscience@gmail.com>
2014-11-08 10:29:51 -0600

    Update frame.h

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -9,9 +9,22 @@
 #include "userprog/pagedir.h"
 #include "userprog/syscall.h"
 
+static struct list frame_list;
+static struct lock frame_lock;
 
-
-
+struct frame{
+  // physical address location
+  void* paddr;
+  // location at virtual address space
+  void* upage;
+  // who owns this frame
+  struct thread* owner;
+  // used for swapping, assign counter during creation?
+  int recent;
+  // for storing it in a list if we need
+  struct list_elem elem;
+  
+};
 
 
 
