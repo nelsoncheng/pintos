d8505e45de5955c19c2c79bccefc610c060f2a6e
kingoffandangos <japesp3@gmail.com>
2014-11-13 13:49:11 -0600

    added ability to save an entire supplemental page into a frame

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -5,7 +5,7 @@ void frame_init(){
   lock_init(&frame_lock);
 }
 
-void * frame_get(void * vpage, bool zero_page){
+void * frame_get(void * vpage, bool zero_page, struct pte * sup_pte){
   void * kpage = palloc_get_page ( PAL_USER | (zero_page ? PAL_ZERO : 0) );
   
   if(kpage == NULL) {
@@ -20,6 +20,7 @@ void * frame_get(void * vpage, bool zero_page){
   new_frame->upage = vpage
   new_frame->owner = thread_current();
   new_frame->pin = false;
+  new_frame->saved_page = sup_pte;
   lock_acquire(&frame_lock);
   list_push_back(&frame_list, new_frame->elem);
   lock_release(&frame_lock);
