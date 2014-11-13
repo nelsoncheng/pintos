60ee8d14b0e2edde07b99a9e925bb13c59293d9d
kingoffandangos <japesp3@gmail.com>
2014-11-13 15:45:03 -0600

    Update swap.c

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,1 +1,17 @@
 #include "vm/swap.h"
+
+
+void swa
+void swap_free_element(struct swap_member *element){
+  lock_acquire(&block_lock);
+  bitmap_set_multiple(free_list, element->address, page_to_sector_ratio, false);
+  lock_release(&block_lock);
+}
+
+void swap_init(){
+    swap_block = block_get_role(BLOCK_SWAP);
+    size = block_size(swap_block);
+    page_to_sector_ratio = PGSIZE / BLOCK_SECTOR_SIZE;
+    free_list = bitmap_create(size);
+    lock_init(&block_lock);
+}
