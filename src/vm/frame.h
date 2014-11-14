e45aa73a9250cbef67e11e89b5e8d744e9e560f5
kingoffandangos <japesp3@gmail.com>
2014-11-14 13:16:02 -0600

    Update frame.h

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -15,12 +15,16 @@
 
 static struct list frame_list;
 static struct lock frame_lock;
+static struct list_elem * next_elem;
 
 void frame_evict();
 void frame_external_lock_acquire();
 void frame_external_lock_release();
 void * frame_get(void * vpage, bool zero_page);
 void frame_init();
+bool frame_free (void * paddr);
+bool frame_pin (void * address, bool user_or_kernal, bool pinval);
+
 
 struct frame{
   // physical address location
