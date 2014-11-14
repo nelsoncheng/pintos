54fa1e09b3e963e8dbb7bd7f16e47f0543e8cd85
kingoffandangos <japesp3@gmail.com>
2014-11-13 19:13:17 -0600

    Update page.c

--- src/vm/page.c
+++ src/vm/page.c
@@ -1,7 +1,7 @@
 #include "vm/page.h"
 #include "threads/malloc.h"
 
-struct pte * page_new(page_type type, int offset, bool read_or_write, struct file * source_file, bool zero){
+struct pte * page_new(page_type type, int offset, bool read_or_write, struct file * source_file, bool zero, size_t read_bytes, size_t zero_bytes){
   
   struct pte new_pte* = (struct pte *) malloc(sizeof(struct pte));
   if (new_pte == NULL){
@@ -13,11 +13,15 @@ struct pte * page_new(page_type type, int offset, bool read_or_write, struct fil
     new_pte->file_offset = NULL;
     new_pte->read_only = NULL;
     new_pte->page_type = ZERO_PAGE;
+    new_pte->bytes_to_read = 0;
+    new_pte->bytes_to_zero = 0;
   } else {
     new_pte->file_ptr = source_file
     new_pte->file_offset = offset
     new_pte->read_only = read_or_write;
     new_pte->page_type = type;
+    new_pte->bytes_to_read = read_bytes;
+    new_pte->bytes_to_zero = zero_bytes;
   }
   new_pte->frame_address = NULL;
   
