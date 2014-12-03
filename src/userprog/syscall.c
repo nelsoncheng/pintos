175957a578db66c9b2184bcf367280746033772e
nelsoncheng <standbackimgoingtotryscience@gmail.com>
2014-12-02 23:35:08 -0600

    Update syscall.c

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -16,5 +16,37 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   printf ("system call!\n");
+  
+  case SYS_CHDIR: 
+    if (!is_valid_pointer(ARG1)){
+  		syscall_exit(-1);
+   	}
+   	status = syscall_chdir((const char *) ARG1);
+   	break;
+  case SYS_MKDIR: 
+    if (!is_valid_pointer(ARG1)){
+  		syscall_exit(-1);
+   	}
+   	status = syscall_mkdir((const char *) ARG1);
+   	break;
+  case SYS_READDIR: 
+    if (!is_valid_pointer(ARG1)){
+  		syscall_exit(-1);
+   	}
+   	status = syscall_readdir((int) ARG1, (char *) ARG2);
+   	break;
+  case SYS_ISDIR: 
+    if (!is_valid_pointer(ARG1)){
+  		syscall_exit(-1);
+   	}
+   	status = syscall_isdir((int) ARG1);
+   	break;
+  case SYS_INUMBER: 
+    if (!is_valid_pointer(ARG1)){
+  		syscall_exit(-1);
+   	}
+   	status = syscall_inumber((int) ARG1);
+    break;
+  
   thread_exit ();
 }
