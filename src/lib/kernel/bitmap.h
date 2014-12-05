2ad478766dd3dace149d5ab76596f0722956a52e
kingoffandangos <japesp3@gmail.com>
2014-12-05 09:24:34 -0600

    Update bitmap.h

--- src/lib/kernel/bitmap.h
+++ src/lib/kernel/bitmap.h
@@ -31,6 +31,7 @@ bool bitmap_contains (const struct bitmap *, size_t start, size_t cnt, bool);
 bool bitmap_any (const struct bitmap *, size_t start, size_t cnt);
 bool bitmap_none (const struct bitmap *, size_t start, size_t cnt);
 bool bitmap_all (const struct bitmap *, size_t start, size_t cnt);
+size_t bitmap_allocate_discontinuous (const struct bitmap *b, size_t cnt, int * sector_positions, int fs_size);
 
 /* Finding set or unset bits. */
 #define BITMAP_ERROR SIZE_MAX
