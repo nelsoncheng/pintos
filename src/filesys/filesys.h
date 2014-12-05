2335fde0bb942b29fe1edf4f9c0f4e443df5a2de
nelsoncheng <standbackimgoingtotryscience@gmail.com>
2014-12-05 07:08:48 -0600

    Update filesys.h

--- src/filesys/filesys.h
+++ src/filesys/filesys.h
@@ -5,6 +5,13 @@
 #include "filesys/off_t.h"
 #include "threads/thread.h"
 #include "threads/malloc.h"
+#include <debug.h>
+#include <stdio.h>
+#include <string.h>
+#include "filesys/file.h"
+#include "filesys/free-map.h"
+#include "filesys/inode.h"
+#include "filesys/directory.h"
 
 /* Sectors of system file inodes. */
 #define FREE_MAP_SECTOR 0       /* Free map file inode sector. */
@@ -22,5 +29,7 @@ struct file *filesys_open (const char *name);
 bool filesys_remove (const char *name);
 directory_token_t filesys_check_path_special_char (const char *path);
 char *filesys_parse_filename (const char *dir);
+struct dir *filesys_parse_dir(const char *direc);
+bool filesys_chdir (const char* name);
 
 #endif /* filesys/filesys.h */
